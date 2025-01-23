e96c16415e98f80c386d2f095ed95ebcd7e0dc95
Ana Lydia Moreira <analydiamoreira965@gmail.com>
2025-01-22 23:01:22 -0300

    test

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.21;
+import {Test} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+    function testMinimunDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+}
